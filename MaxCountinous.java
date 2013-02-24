8fa77a8b566870880ce40909c94b620b2c25eadf
Yan Wang <yw229@njit.edu>
2013-02-23 22:50:33 -0500

    find the max sum of subsequence of array using DP

--- MaxCountinous.java
+++ MaxCountinous.java
@@ -1,6 +1,13 @@
 package LinkedIn;
 
-
+/*
+ * Dynamic Programming 
+ * Find the max sum of any sub array in the range of array[0,n-1] (zero/negative/positive)
+ * subproblem find max sum of any sub array starting at any index in the range [0,i] end at index i 
+ * array contains negative,positive, sub[i] = max {sum[i-1] + a[i],0}, if after adding next sum value is negative, which means does not
+ * contribute to the sub array; for each intermediate result sub[0]...sub[i] we find the max value 
+ * 
+ * */
 
 public class MaxCountinous {
 	public int[] array ;
@@ -34,27 +41,31 @@ public class MaxCountinous {
 		}
 		return max ; */
 		this.array = array ;
-		int temp = 0 ;
+		int current_max_so_far = 0 ;
+		int current_index = 0 ;
 	      //  max = 0 ;
 	        for (int i = 0 ; i < array.length; i ++)
 	        {
-	            temp += array[i] ;
-	            if (max < temp)
-	              {
-	                max = temp ; 
-	                endpoint = i ;
-	               }
-	            if(temp<0)
-	            {
-	               temp = 0 ;
-	               startpoint= endpoint ; 
-	               System.out.println("startPoint" + startpoint) ;
-	               
-	            }
-	               
+	        	
+	           // current_max_so_far = Math.max(0, current_max_so_far + array[i]) ;
+	          //  max = Math.max(current_max_so_far, max) ;
+	        	current_max_so_far = current_max_so_far+ array[i] ; 
+	        	if(current_max_so_far <0)
+	        	{
+	        		current_max_so_far = 0 ;
+	        		current_index= i+1 ; // store the startpointer 
+	        	}
+	        	else
+	        		if(current_max_so_far > max)
+	        		{
+	        			max = current_max_so_far ;
+	        			endpoint = i ;
+	        			startpoint = current_index ;
+	        		}
 	        }
 	        return max ; 
 	}
+	
 	public void initialize()
 	{
 		max = 0 ;
@@ -80,7 +91,7 @@ public class MaxCountinous {
 	}
 	public static void main (String[] args)
 	{
-		int a[] = {3,-1,2,-1,-3} ;
+		int a[] = {-9,-3,33,-6,27} ;
 		MaxCountinous max = new MaxCountinous() ;
 		max.initialize(); 
 		max.max(a) ;
